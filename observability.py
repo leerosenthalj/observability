ae2f300de5c54941f882ab586d402268c20f7a3a
Lee J. Rosenthal <leerosenthalj@gmail.com>
2019-06-04 15:15:21 -0700

    Wrote outline, and renamed file to observability.py.

---
+++ observability.py
@@ -0,0 +1,34 @@
+import ephem
+import numpy as np
+from scipy import ndimage
+import pylab as py
+import matplotlib.dates as mdates
+import pdb
+
+'''Outline:
+makeplot
+    Put together all of the functions listed below.
+drawplot
+    Takes dates/times/visibility-map, returns a figure.
+observable
+    Make map of observability.
+numeric
+    Test whether input is numeric.
+target
+    set up target location in proper units.
+observatory
+    Set up PyEphem 'observer object'. Hardcode a bunch of options.
+hms
+    Convert between hour/minute/second and decimal degree.
+dms
+    Convert between degree/minute/second and decimal degree.
+
+'''
+Class 
+
+
+def makeplot(obs, ra, dec, minElev=30, twilight=12, oversamp=16, dt=0):
+    pass
+
+def drawplot():
+    pass
